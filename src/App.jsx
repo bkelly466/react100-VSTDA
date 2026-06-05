5414c49534251db7072a00bdaa6f5e3604928ef1
Brandon K <brandon@Brandons-MacBook-Air.local>
2026-06-05 19:07:58 -0400

    Fixed todo form, work on todo list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {useState} from "react";
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   // States held here
@@ -15,7 +16,7 @@ function App() {
 
     setTodo([...todo, newTodo]);
   };
-  
+
   return (
     <>
       <div className="container my-5 text-dark">
@@ -32,14 +33,7 @@ function App() {
           </div>
 
           <div className="col-md-8">
-            <div className="card h-100 shadow-sm border-0">
-              <h2 className="card-header bg-light py-3 border-bottom-0">View Todos</h2>
-              <div className="card-body p-4 text-center text-muted">
-                <div className="py-5 bg-light rounded border border-dashed">
-                  <p className="mb-0">No tasks found. Add a task to get started!</p>
-                </div>
-              </div>
-            </div>
+            <TodoList todos={todo} />
           </div>
         </div>
       </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,7 +3,25 @@ import TodoItem from './TodoItem';
 function TodoList({ todos, updateTodo, deleteTodo }) {
   return (
     <>
-      {/* Add your components here */}
+      <div className="card h-100 shadow-sm border-0">
+        <h2 className="card-header bg-light py-3 border-bottom-0">View Todos</h2>
+        <div className="card-body p-4 text-center text-muted">
+          <div className="py-5 bg-light rounded border border-dashed">
+            {todos.length === 0 ? (
+              <p className="mb-0">No tasks found. Add a task to get started!</p>
+            ) : (
+              todos.map((todo) => (
+                <TopSpot
+                    id={todo.id}
+                    text={todo.text}
+                    priority={todo.priority}
+                />
+            ))
+            )
+            }
+          </div>
+        </div>
+      </div>
     </>
   );
 }
