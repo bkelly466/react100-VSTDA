b2aa766bf5526a0a15e65e1dc2568a8060e685de
Brandon Kelly <brandonkelly466@gmail.com>
2026-06-05 19:55:36 -0400

    Small change to HTML

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,20 +5,25 @@ function TodoList({ todos, updateTodo, deleteTodo }) {
     <>
       <div className="card h-100 shadow-sm border-0">
         <h2 className="card-header bg-light py-3 border-bottom-0">View Todos</h2>
-        <div className="card-body p-4 text-center text-muted">
-          <div className="py-5 bg-light rounded border border-dashed">
-            {todos.length === 0 ? (
+        <div className="card-body p-4 text-start text-muted">
+          
+          {todos.length === 0 ? (
+            <div className="py-5 bg-light rounded border border-dashed text-center">
               <p className="mb-0">No tasks found. Add a task to get started!</p>
-            ) : (
-              todos.map((todo) => (
+            </div>
+          ) : (
+            <div className="todo-list-container">
+              {todos.map((todo) => (
                 <TodoItem
                     key={todo.id}
                     todo={todo}
                     updateTodo={updateTodo}
                     deleteTodo={deleteTodo}
                 />
-            )))}
-          </div>
+              ))}
+            </div>
+          )}
+          
         </div>
       </div>
     </>
